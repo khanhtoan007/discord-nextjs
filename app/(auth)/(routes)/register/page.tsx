e8fba0509d674d36e19eb15ba908166aab511fe3
TonyToan <toantdkde160402@fpt.edu.vn>
2023-11-12 22:49:09 +0700

    base file

---
+++ app/(auth)/(routes)/register/page.tsx
@@ -0,0 +1,10 @@
+const RegisterPage = () => {
+    return (
+      <div>
+        <p className='text-3xl font-bold'>Test</p>
+      </div>
+    )
+  }
+  
+  export default RegisterPage;
+  
---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,8 @@
+const AuthLayout = ({ children } : {childrend: React.ReactNode }) => {
+  return (
+    <div className="bg-red-400 h-full">
+        {children}
+    </div>
+  );
+}
+export default AuthLayout;
