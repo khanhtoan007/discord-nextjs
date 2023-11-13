16e4ba31f682d7447e09acd4220f4a4173abcbde
TonyToan <toantdkde160402@fpt.edu.vn>
2023-11-13 22:22:57 +0700

    initial ui

--- app/(setup)/page.tsx
+++ app/(setup)/page.tsx
@@ -1,6 +1,7 @@
 import { initialProfile } from "@/lib/initial-profile";
 import {db} from "@/lib/db";
 import { redirect } from "next/navigation";
+import { InitialModal } from "@/components/modals/initial-modal";
 const SetupPage =async () => {
     const profile = await initialProfile();
     const server = await db.server.findFirst({
@@ -18,7 +19,7 @@ const SetupPage =async () => {
     }
 
 
-    return <div>Create a Serve</div>;
+    return <InitialModal />;
 }
 
 export default SetupPage;
